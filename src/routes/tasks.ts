1b9169f519ffddd708addc66c5168fbf6eccb30b
Mekuanint Abebe <se.mekuannint.abebe@gmail.com>
2023-08-16 12:58:58 +0300

    mark as complete

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -3,13 +3,19 @@ import {
   getTasks,
   getTask,
   createTask,
-  deleteTask, 
-  updateTask
+  deleteTask,
+  updateTask,
+  markTaskAsComplete,
 } from "../controllers/tasks";
 const router = express.Router({ mergeParams: true });
 const { protect } = require("../middlewares/auth");
 
 router.route("/").get(protect, getTasks).post(protect, createTask);
-router.route("/:id").get(protect, getTask).put(protect, updateTask).delete(protect, deleteTask);
+router
+  .route("/:id")
+  .get(protect, getTask)
+  .put(protect, updateTask)
+  .delete(protect, deleteTask);
+router.route("/:id/complete").put(protect, markTaskAsComplete);
 
 export default router;
